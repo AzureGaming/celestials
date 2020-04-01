f45f3a9e0b0fdb315d486b410c3504ab42fbb908
Andrew Lam <drewclam@hotmail.com>
2020-04-01 17:58:02 -0400

    Fix Selection prompt hide

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -27,6 +27,6 @@ public class CardManager : MonoBehaviour {
         //    return cards[randomIndex];
         //}
         //return GetValidCardData();
-        return cards[0];
+        return cards[1];
     }
 }
