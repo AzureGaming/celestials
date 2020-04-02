9a02e8722ca3b869dd4f6db2c2402813bb2b5273
Andrew Lam <drewclam@hotmail.com>
2020-04-01 20:08:00 -0400

    Update pix sprite

---
+++ Assets/Scripts/ManaDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManaDisplay : MonoBehaviour {
+    TextMeshProUGUI currentMana;
+    TextMeshProUGUI maxMana;
+
+    private void Awake() {
+        TextMeshProUGUI[] refs = GetComponentsInChildren<TextMeshProUGUI>();
+        currentMana = refs[0];
+        maxMana = refs[1];
+    }
+
+    public void UpdateCurrentMana(int mana) {
+        currentMana.text = mana.ToString();
+    }
+
+    public void UpdateMaxMana(int mana) {
+        maxMana.text = mana.ToString();
+    }
+}
