ea6ea69813aa52a7057c65df3508fb12051d49d0
Andrew Lam <drewclam@hotmail.com>
2020-07-16 20:34:51 -0400

    Add paradox spell

---
+++ Assets/Scripts/Spells/Paradox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paradox : CardEffect {
+    CardManager cardManager;
+
+    private void Awake() {
+        cardManager = FindObjectOfType<CardManager>();
+    }
+
+    public override IEnumerator Apply() {
+        yield return StartCoroutine(ParadoxRoutine());
+    }
+
+    IEnumerator ParadoxRoutine() {
+        Card[] cards = cardManager.GetCardsInHand();
+        foreach (Card card in cards) {
+            card.SetManaCost(card.GetManaCost() - 1);
+        }
+        yield break;
+    }
+}
