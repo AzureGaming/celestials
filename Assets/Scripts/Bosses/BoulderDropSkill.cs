5e9cf11e90ff75dd3d23a51f4974eccd1f718e5d
Andrew Lam <drewclam@hotmail.com>
2020-07-23 20:51:18 -0400

    Add boulder drop queueing

---
+++ Assets/Scripts/Bosses/BoulderDropSkill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderDropSkill : MonoBehaviour {
+    public EarthElementalSkillIndicators skillIndicators;
+    public AttackQueueManager queueManager;
+    public Animator animator;
+    CardManager cardManager;
+    GameManager gameManager;
+    public GameObject boulder;
+    public GameObject boulderSpawner;
+    Summoner summoner;
+
+    bool isCasting;
+
+    private void Awake() {
+        cardManager = FindObjectOfType<CardManager>();
+        gameManager = FindObjectOfType<GameManager>();
+        summoner = FindObjectOfType<Summoner>();
+    }
+
+    public void QueueSkill() {
+        skillIndicators.SetBoulderDrop();
+        queueManager.Queue(new AttackQueueManager.AttackCommand(EarthElemental.Moves.BOULDERDROP));
+    }
+
+    public void OnCastAnimationEnd() {
+        isCasting = false;
+    }
+
+    public IEnumerator CastSkill() {
+        isCasting = true;
+        animator.SetTrigger("Attack1");
+        yield return new WaitUntil(() => !isCasting);
+        yield return StartCoroutine(BoulderDrop());
+        skillIndicators.ClearIndicator();
+    }
+
+    IEnumerator BoulderDrop() {
+        gameManager.SetWaitForCompletion(true);
+        Instantiate(boulder, boulderSpawner.transform);
+        yield return new WaitUntil(() => DoneBoulderDrop());
+        yield return StartCoroutine(summoner.TakeDamage(3));
+    }
+
+    bool DoneBoulderDrop() {
+        return !gameManager.GetWaitForCompletion();
+    }
+}
