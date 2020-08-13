88df5b61f238ce1ca5ab49bf188b96cbc54590a6
solofhreaper <61039567+solofhreaper@users.noreply.github.com>
2020-08-12 21:52:53 -0400

    Accelerate Prefab and some other stuff I guess

--- Assets/Scripts/SpellEffects/ParadoxEffect.cs
+++ Assets/Scripts/SpellEffects/ParadoxEffect.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParadoxEffect : MonoBehaviour
+public class ParadoxEffect : SpellEffect
 {
     // Start is called before the first frame update
     void Start()
